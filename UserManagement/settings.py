28f375c618c1cb3e5826342774e3a1742ebea62d
subhadarshani <subhadarshani82@gmail.com>
2024-12-01 18:32:01 +0530

    added Signup model to store user data.

--- UserManagement/settings.py
+++ UserManagement/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'UserManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user_db',
+        'USER':'root',
+        'PASSWORD':'admin'
+        
     }
 }
 
--- signupApp/models.py
+++ signupApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Signup(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    email=models.EmailField()
+    password=models.CharField(max_length=50)
+    contact =models.IntegerField
+    age=models.IntegerField()
+    dob = models.DateField()
+    gender = models.CharField(max_length=50)
+    language=models.CharField(max_length=50)
+    qualigication =models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
